2c64593cb3199e0caf8ef253e914b9075cb2c603
Peng Wang <peng@pengxwang.com>
2017-05-07 17:46:12 -0700

    Basic TabBarController, integration, hidden bar

---
+++ UITester/TabBarController.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarController.swift
+//  UITester
+//
+//  Copyright (c) 2015-present Peng Wang. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+}
